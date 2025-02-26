a097bf5c00f65a6be71af16942c848e000428cac
mzerhusen <zerhusma@mail.uc.edu>
2025-02-25 23:21:00 -0500

    Initial Die Roller

---
+++ src/DieRoller.java
@@ -0,0 +1,35 @@
+import java.util.Random;
+
+public class DieRoller
+{
+    public static void main(String[] args)
+    {
+        int dieOne;
+        int dieTwo;
+        int dieThree;
+        int rollNumber=1;
+        boolean dieMatch = false;
+
+        System.out.println("Roll \tDie 1 \tDie 2 \tDie 3");
+        do
+        {
+            dieOne= new Random().nextInt(6) + 1;
+            dieTwo= new Random().nextInt(6) + 1;
+            dieThree= new Random().nextInt(6) + 1;
+            System.out.printf("%1d \t%1d \t%1d \t%1d\n", rollNumber, dieOne, dieTwo, dieThree);
+            if (dieOne==dieTwo && dieOne==dieThree)
+            {
+                dieMatch = true;
+            }
+            else
+            {
+                dieMatch = false;
+                rollNumber++;
+            }
+        }
+        while(!dieMatch);
+
+
+
+    }
+}

95b1165ba75d529f441b5a9eda5c2ad9ac9eaf17
mzerhusen <zerhusma@mail.uc.edu>
2025-02-26 21:58:26 -0500

    Update

--- src/PartA.java
+++ src/PartA.java
@@ -4,22 +4,22 @@ public class PartA
     {
         for(int c=0; c <= 30; c++)
         {
-            System.out.print(c + ", ");
+            System.out.print(c + ". ");
         }
         System.out.println();
         for(int c=30; c >= 0; c-=1)
         {
-            System.out.print(c + ", ");
+            System.out.print(c + ". ");
         }
         System.out.println();
         for(int c=0; c <= 18; c+=3)
         {
-            System.out.print(c + ", ");
+            System.out.print(c + ". ");
         }
         System.out.println();
         for(int c=10; c >= 0; c-= 2)
         {
-            System.out.print(c + ", ");
+            System.out.print(c + ". ");
         }
     }
 }

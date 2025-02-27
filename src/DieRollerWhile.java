73ef82d4bb338682dcd7a2b78de2cd6858d9156d
mzerhusen <zerhusma@mail.uc.edu>
2025-02-26 22:24:04 -0500

    Update

--- src/DieRollerWhile.java
+++ src/DieRollerWhile.java
@@ -1,24 +1,58 @@
 import java.util.Random;
+import java.util.Scanner;
 
 public class DieRollerWhile
 {
     public static void main(String[] args)
     {
         //Set variables for Die
-        int dieOne = 1;
+        Scanner in = new Scanner(System.in);
+        Random dice = new Random();
+        int dieOne;
         int dieTwo = 2;
         int dieThree = 3;
-        int rollNumber = 1;
+        int rollNumber;
+        String rollAgain;
+        boolean validRollAgain;
+        boolean playAgain = true;
 
         //Set up table format
-        System.out.println("Roll \tDie 1 \tDie 2 \tDie 3");
-        while(dieOne != dieTwo || dieOne != dieThree)
+        while(playAgain)
         {
-            dieOne = new Random().nextInt(6) + 1;
-            dieTwo = new Random().nextInt(6) + 1;
-            dieThree = new Random().nextInt(6) + 1;
-            System.out.printf("%1d \t\t %1d \t\t %1d \t\t%1d \n", rollNumber, dieOne, dieTwo, dieThree);
-            rollNumber++;
+            dieOne = 0;
+            rollNumber = 1;
+            System.out.println("Roll \tDie 1 \tDie 2 \tDie 3");
+            while (dieOne != dieTwo || dieOne != dieThree) {
+                dieOne = dice.nextInt(6) + 1;
+                dieTwo = dice.nextInt(6) + 1;
+                dieThree = dice.nextInt(6) + 1;
+                System.out.printf("%1d \t\t %1d \t\t %1d \t\t%1d \n", rollNumber, dieOne, dieTwo, dieThree);
+                rollNumber++;
+            }
+            System.out.print("Would you like to roll again? Y/N ");
+            do
+            {
+                rollAgain = in.nextLine();
+                rollAgain=rollAgain.toUpperCase();
+                if (rollAgain.equals("Y") || rollAgain.equals("N"))
+                {
+                    validRollAgain = true;
+                }
+                else
+                {
+                    validRollAgain = false;
+                }
+            }
+            while(!validRollAgain);
+            if (rollAgain.equals("Y"))
+            {
+                playAgain = true;
+            }
+            else
+            {
+                playAgain = false;
+            }
         }
+        System.out.println("Have a great day!");
     }
 }
